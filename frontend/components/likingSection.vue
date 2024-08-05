1a290bed597cfc821327fb08cc4b0e9f899e4679
Ivan Kalashnikov <ivank.public@gmail.com>
2024-08-05 20:15:53 +0700

    (dev) - liking component and page.

---
+++ frontend/components/likingSection.vue
@@ -0,0 +1,45 @@
+<template>
+  <v-card class="mx-auto">
+    <v-card-text>
+      <h2 class="text-h5 mb-2">{{ title }}</h2>
+
+      <v-chip-group
+        v-model="selectedItems"
+        column
+        multiple
+      >
+        <v-chip
+          v-for="(item, index) in items"
+          :key="index"
+          :text="item.text"
+          :value="item.value"
+          variant="outlined"
+          filter
+        >
+          {{ item.text }}
+        </v-chip>
+      </v-chip-group>
+    </v-card-text>
+  </v-card>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      selectedItems: [],
+    };
+  },
+
+  props: {
+    title: {
+      type: String,
+      default: ''
+    },
+    items: {
+      type: Array,
+      default: () => []
+    }
+  }
+}
+</script>
--- frontend/pages/liking.vue
+++ frontend/pages/liking.vue
@@ -1,5 +1,48 @@
 <template>
-  <section>
-    <p>This page will be displayed at the /linking route.</p>
-  </section>
+  <div class="liking-group-container">
+    <h1>Постоянные предпочтения</h1>
+    <liking-section
+      title="Категории"
+      :items=categoryItems
+    >
+    </liking-section>
+    <liking-section
+      title="Стиль"
+      :items=styleItems
+    >
+    </liking-section>
+  </div>
 </template>
+
+<script>
+import likingSection from "@/components/likingSection";
+
+export default {
+  components: {
+    likingSection,
+  },
+  data() {
+    return {
+      categoryItems: [
+        { text: 'Кафе', value: 1 },
+        { text: 'Рестораны', value: 2 },
+        { text: 'Парки', value: 3 },
+        { text: 'Достопримечательности', value: 4 },
+        { text: 'Красота', value: 5 },
+        { text: 'Развлечения', value: 6 },
+        { text: 'Шопинг', value: 7 },
+        { text: 'Анти-кафе', value: 8 },
+      ],
+
+      styleItems: [
+        { text: 'Детский', value: 1 },
+        { text: 'Взрослый', value: 2 },
+        { text: 'Шумный', value: 3 },
+        { text: 'Спокойный', value: 4 },
+        { text: 'Для компании', value: 5 },
+        { text: 'Одному', value: 6 },
+      ],
+    };
+  },
+}
+</script>
