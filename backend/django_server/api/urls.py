528e712a715ce46c457052c87e8b1df0d059a010
Marta Reed <martaree@student.21-school.ru>
2024-08-07 11:35:49 +0700

    (dev) - add mock GET endpoints users/ && users/:id/

--- backend/django_server/api/urls.py
+++ backend/django_server/api/urls.py
@@ -1,10 +1,11 @@
 from django.urls import include, path
 from rest_framework import routers
 
-from .views import HelloView
+from .views import HelloView, UsersView
 
 router = routers.DefaultRouter()
 router.register(r'hello', HelloView, basename='hello')
+router.register(r'users', UsersView, basename='users')
 
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browsable API.
--- backend/django_server/api/views.py
+++ backend/django_server/api/views.py
@@ -5,4 +5,25 @@ from rest_framework import status
 class HelloView(ViewSet):
     def list(self, requet):
         return Response({"message": "hello"}, status=status.HTTP_200_OK)
+    
+#Users
+class UsersView(ViewSet):
+    def list(self, request):
+        # TMP Data
+        users = [
+            {"id": 1, "username": "awesome_user", "tags": ["Пицца", "Барбершоп", "Животные"]},
+            {"id": 2, "username": "super_user", "tags": ["Вино", "Грузинская кухня", "Музей", "Театр"]},
+        ]
+        return Response(users, status=status.HTTP_200_OK)
+    
+    def retrieve(self, request, pk=None):
+        users = {
+            1: {"id": 1, "username": "awesome_user", "tags": ["Пицца", "Барбершоп", "Животные"]},
+            2: {"id": 2, "username": "super_user", "tags": ["Вино", "Грузинская кухня", "Музей", "Театр"]},
+        }
+        user = users.get(int(pk))
+        if user:
+            return Response(user, status=status.HTTP_200_OK)
+
+        return Response({"detail": "User not found"}, status=status.HTTP_404_NOT_FOUND)
 
