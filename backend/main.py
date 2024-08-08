0ba5b5c79e6d05bf3d289e1c989431f014a76571
Ivan Kalashnikov <ivank.public@gmail.com>
2024-08-08 16:46:28 +0700

    (dev) - api module and routers.

--- backend/main.py
+++ backend/main.py
@@ -1,13 +1,5 @@
-from typing import Union
-
 from app import app
+from api.api import api_router
 
 
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
-
-
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
+app.include_router(api_router, prefix="/api")
