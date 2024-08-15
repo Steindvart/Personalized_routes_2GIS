9aed866f20e096af88743db5c49fe9e19980e4b3
Ivan Kalashnikov <ivank.public@gmail.com>
2024-08-15 12:43:28 +0700

    (dev) - journey page and route info component init.

---
+++ frontend/components/routeInfoCard.vue
@@ -0,0 +1,60 @@
+<template>
+  <v-card class="mx-auto" max-width="600" subtitle="Места, которые подходят под ваши активности и предпочтения" outlined>
+    <template v-slot:title>
+      <span class="font-weight-black">Маршрут путешествия</span>
+    </template>
+    <v-card-text>
+      <v-timeline density="compact" side="end" align="start">
+        <v-timeline-item
+          v-for="(item, i) in items"
+          :key="i"
+          :dot-color="item.color"
+          :icon="item.icon"
+          fill-dot
+          size="large"
+        >
+          <v-card>
+            <v-card-title :class="['text-h6', `bg-${item.color}`]">
+              Lorem Ipsum Dolor
+            </v-card-title>
+            <v-card-text class="bg-white text--primary">
+              <p>Lorem ipsum dolor sit amet, no nam oblique veritus.</p>
+            </v-card-text>
+          </v-card>
+        </v-timeline-item>
+      </v-timeline>
+
+      <!-- Кнопка "Начать путешествие" по центру внизу элементов -->
+      <div class="start-journey-container">
+        <v-btn color="teal" @click="startJourney" class="start-journey-btn" variant="outlined" rounded="xl" size="large">
+          Перенастроить маршрут
+        </v-btn>
+      </div>
+    </v-card-text>
+  </v-card>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    items: [
+      {
+        color: 'red-lighten-2',
+        icon: 'mdi-star',
+      },
+      {
+        color: 'purple-lighten-2',
+        icon: 'mdi-book-variant',
+      },
+      {
+        color: 'green-lighten-1',
+        icon: 'mdi-airballoon',
+      },
+      {
+        color: 'indigo-lighten-2',
+        icon: 'mdi-layers-triple',
+      },
+    ],
+  }),
+}
+</script>
---
+++ frontend/pages/journey.vue
@@ -0,0 +1,86 @@
+<template>
+  <v-container class="full-width-container">
+    <h1>Умные путешествия</h1>
+    <br/>
+    <div class="content-wrapper">
+      <route-info-card
+        class="settings-card"
+        :point="selectedPoint"
+      >
+      </route-info-card>
+      <v-card class="map-container" outlined>
+        <template v-slot:title>
+          <span class="font-weight-black">Навигация</span>
+        </template>
+        <v-card-text>
+          <t-gis-map
+            :center="[82.89785, 54.98021]"
+            :styles="{width: '100%', height: '870px'}"
+            :zoom="12"
+            :selectedPoint="selectedPoint"
+          ></t-gis-map>
+        </v-card-text>
+      </v-card>
+    </div>
+    <br/>
+  </v-container>
+</template>
+
+
+<script>
+import routeInfoCard from '@/components/routeInfoCard'
+import { ref } from 'vue'
+
+export default {
+  name: 'routePage',
+  components: {
+    routeInfoCard
+  },
+
+  setup() {
+    const selectedPoint = ref([0, 0])
+
+    return { selectedPoint }
+  }
+}
+</script>
+
+<style scoped>
+.full-width-container {
+  max-width: 100% !important; /* Убираем ограничение по ширине */
+  width: 100%; /* Занимаем всю ширину экрана */
+  padding: 0; /* Убираем лишние отступы */
+}
+
+.content-wrapper {
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: stretch;
+}
+
+.settings-card {
+  flex: 1 1 60%; /* Гибкий размер, 60% ширины от родителя */
+  margin-right: 16px; /* Отступ между картой и настройками */
+  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+  background-color: #fff;
+}
+
+.map-container {
+  flex: 2 1 70%; /* Гибкий размер, 70% ширины от родителя */
+  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+  height: 100%; /* Занимает всю высоту родителя */
+  display: flex; /* Гарантируем, что вложенные элементы будут растягиваться */
+  flex-direction: column;
+}
+
+.map-container > .v-card-text {
+  flex-grow: 1; /* Внутренний контент растягивается */
+  padding: 0; /* Убираем лишние отступы */
+}
+
+.t-gis-map {
+  width: 100%;
+  height: 100%;
+}
+</style>
