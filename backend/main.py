a27b93d43519f180490f72216b32f698d081241b
jaycemarpeer <jaycemar@bk.ru>
2024-08-15 18:51:46 +0700

    changes imports and the addition of exception handlers in main.py

--- backend/main.py
+++ backend/main.py
@@ -1,10 +1,12 @@
 from app.api.routes import router as api_router
 
-from app.exceptions.handlers import not_found_exception_handler
-from app.exceptions.handlers import bad_request_exception_handler
-from app.exceptions.handlers import generic_exception_handler
-from app.exceptions.handlers import NotFoundException
-from app.exceptions.handlers import BadRequestException
+from app.exceptions.handlers import (
+    not_found_exception_handler,
+    bad_request_exception_handler,
+    generic_exception_handler,
+    NotFoundException,
+    BadRequestException,
+)
 
 from app.config import app
 
@@ -13,6 +15,11 @@ import logging as log
 log.debug('app run')
 app.include_router(api_router, prefix="/api")
 
-app.add_exception_handler(NotFoundException, not_found_exception_handler)
-app.add_exception_handler(BadRequestException, bad_request_exception_handler)
-app.add_exception_handler(Exception, generic_exception_handler)
+exception_handlers = {
+    NotFoundException: not_found_exception_handler,
+    BadRequestException: bad_request_exception_handler,
+    Exception: generic_exception_handler,
+}
+
+for exc, handler in exception_handlers.items():
+    app.add_exception_handler(exc, handler)
