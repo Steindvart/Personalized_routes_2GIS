2aa4b11093215b4882bd32a5989d839159fcc12e
Ivan Kalashnikov <ivank.public@gmail.com>
2024-08-16 15:01:38 +0700

    (dev) - launch uvicorn from main.py for debugging.

--- backend/main.py
+++ backend/main.py
@@ -1,3 +1,5 @@
+import uvicorn
+
 from app.api.routes import router as api_router
 from app.config import app
 
@@ -5,3 +7,7 @@ import logging as log
 
 log.debug('app run')
 app.include_router(api_router, prefix="/api")
+
+
+if __name__ == "__main__":
+  uvicorn.run(app, host="127.0.0.1", port=8000)
