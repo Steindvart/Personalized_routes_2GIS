705cebfb63ea951c240f3fbff0aec4601532a429
Georgiy-JO <wkone.mail@gmail.com>
2024-08-16 08:01:17 +0700

    Site looks fine. Next: APIs integration and finish points

---
+++ frontend/components/infoGPTCard.vue
@@ -0,0 +1,180 @@
+<template>
+    <v-card class="mx-auto" max-width="600" outlined>
+      <v-card-subtitle class="subtitle_text">
+        Укажи ваше местоположение и удобный вам радиус поиска
+      </v-card-subtitle>
+
+      <v-card-text>
+        <v-container>
+            <v-text-field
+                v-model="latitudeInput"
+                label="Широта"
+                outlined
+                class="input_field"
+                clearable
+            ></v-text-field>
+        </v-container>
+        <v-container>
+            <v-text-field
+                v-model="longitudeInput"
+                label="Долгота"
+                outlined
+                class="input_field"
+                clearable
+            ></v-text-field>
+        </v-container>
+        <v-container>
+            <v-text-field
+                v-model="radiusInput"
+                label="Радиус поиска"
+                outlined
+                class="input_field"
+                clearable
+            ></v-text-field>
+        </v-container>
+        <v-row class="mt-4 justify-center">
+            <v-col cols="auto">
+                <!-- Button to find the place in map -->
+                <v-btn color="primary" @click="addActivity" class="show_in_map_btn" variant="outlined" rounded="xl" size="x-large">
+                Покажи на карте
+                </v-btn>
+            </v-col>
+        </v-row>
+        <v-container >
+            <v-row class="justify-center">
+                <v-col cols="auto" >
+                    <!-- Button to find get info from GPT -->
+                    <v-btn color="primary" @click="tellTheStory" class="tell_the_story_btn" variant="outlined" rounded="xl" size="x-large">
+                    Расскажи историю
+                    </v-btn>
+                </v-col>
+            </v-row>
+            <div class="story_text">{{ storyText }}</div>
+        </v-container>
+
+  
+      </v-card-text>
+    </v-card>
+  </template>
+  
+  <script>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  import axios from 'axios'
+  
+  export default {
+    props: {
+      point: {
+        type: Array,
+        default: () => [0, 0],
+      },
+    },
+
+
+
+
+
+
+
+
+
+    data() {
+        return {
+          latitudeInput: '',
+          longitudeInput: '',
+          radiusInput: '',
+          storyText: 'This is where the text will be displayed.'
+        }
+    },
+    methods: {
+        tellTheStory() {
+        this.storyText = 'The story!'
+        }
+    },
+
+
+
+
+
+
+
+  }
+  </script>
+  
+  <style scoped>
+  .activity-number {
+    width: 20px;
+    text-align: right;
+    margin-right: 10px;
+  }
+  
+  .start-journey-container {
+    display: flex;
+    justify-content: center;
+    margin-top: 20px;
+  }
+  
+  .v-card {
+    position: relative;
+  }
+
+
+
+
+
+
+
+
+  .show_in_map_btn {
+    text-transform: none;
+  }
+  .tell_the_story_btn {
+    text-transform: none;
+  }
+  .subtitle_text {
+  word-wrap: break-word;
+  white-space: normal;
+  }
+  .input_field {
+  height: 20px; 
+  }
+  .story_text{
+    font-size: 16px; /* Customize font size */
+    padding: 5px;
+    /* border: 1px solid #ccc;*/
+    background-color: #f9f9f931;
+  }
+
+
+
+  </style>
+  
---
+++ frontend/pages/guide.vue
@@ -0,0 +1,68 @@
+<template>
+  <div> 
+    <h2>GPT-основанный гайд</h2>
+    <v-container class="content_wrapper">
+        <info-card class="info_card":point="selectedPoint">    
+        </info-card>
+        <v-card class="map_container">
+            <template v-slot:title>
+                <span class="font-weight-black">Карта</span>
+            </template>
+            <t-gis-map
+                :center="[82.89785, 54.98021]"
+                :styles="{width: '100%', height: '870px'}"
+                :zoom="12"
+                :selectedPoint="selectedPoint">
+            </t-gis-map>
+        </v-card>
+    </v-container>
+  </div>
+    
+</template>
+
+
+
+<script>
+import infoCard from '@/components/infoGPTCard'
+import { ref } from 'vue'
+
+export default {
+  name: 'indexPage',
+  components: {
+    infoCard
+  },
+
+  setup() {
+    const selectedPoint = ref([0, 0])
+
+    return { selectedPoint }
+  }
+}
+</script>
+
+
+
+<style scoped>
+.content_wrapper {
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: stretch;
+}
+
+.info_card {
+  flex: 1 1 30%;
+  margin-right: 16px; /* Отступ между картой и настройками */
+  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+  background-color: #fff;
+
+}
+.map_container {
+  flex: 2 1 60%;
+  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+  height: 100%; /* Занимает всю высоту родителя */
+  display: flex; /* Гарантируем, что вложенные элементы будут растягиваться */
+  flex-direction: column;
+}
+
+</style>
