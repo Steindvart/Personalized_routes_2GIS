152297ade416eb895d40534d723c631cac4ace96
Georgiy-JO <wkone.mail@gmail.com>
2024-08-17 19:56:21 +0700

    Guide_V.2.1 Hardcoded radius.

--- frontend/components/infoGPTCard.vue
+++ frontend/components/infoGPTCard.vue
@@ -10,26 +10,6 @@
       </p>
 
       <v-card-text>
-      <!--<v-card-subtitle class="subtitle_text">
-        Укажи ваше местоположение и интересуеющее вас расстояние 
-      </v-card-subtitle>
-         <v-container>
-          <p class="input_field">Широта: {{ point[0] }}</p><p class="input_field"> Долгота: {{point[1] }}</p>
-            <v-text-field
-                v-model="localradius"
-                label="Радиус поиска"
-                outlined
-                class="input_field"
-                clearable
-            ></v-text-field>
-        </v-container> 
-        <v-row class="mt-4 justify-center">
-            <v-col cols="auto">
-                <v-btn color="primary" @click="fixRadius" class="show_on_map_btn" variant="outlined" rounded="xl" size="x-large">
-                  Зафиксировать радиус
-                </v-btn>
-            </v-col>
-        </v-row>-->
         <v-container >
             <v-row class="justify-center">
                 <v-col cols="auto" >
@@ -77,8 +57,6 @@ import { Text } from 'vue';
         // OUTPUT OF GPT API GOES HERE!
         },
 
-
-
         fixRadius() {
         if(this.point[0] !==0 && this.point[1] !==0){
           this.$emit('radius-updated', {
@@ -91,9 +69,6 @@ import { Text } from 'vue';
   </script>
   
   <style scoped>
-  .show_on_map_btn {
-    text-transform: none;
-  }
   .tell_the_story_btn {
     text-transform: none;
   }
@@ -106,13 +81,8 @@ import { Text } from 'vue';
   font-family: 'Roboto', sans-serif;
   max-width: 1000px;
   padding: 5px;
+  }
 
-  }
-  .input_field {
-    font-size: 16px;
-    height: 40px; 
-  }
-  
   .story_text{
     font-size: 16px; /* Customize font size */
     padding: 5px;
