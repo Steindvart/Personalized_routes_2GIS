1e3a33ffd509dbd27bf037dcd0b62df9e8e34580
Ivan Kalashnikov <ivank.public@gmail.com>
2024-08-19 10:37:46 +0700

    (fix) - remove blocked api key.

--- backend/app/api/similar_top.py
+++ backend/app/api/similar_top.py
@@ -1,13 +1,12 @@
 import pandas as pd
 import difflib
-import json
 
 import requests
 
 
 base_url = "https://catalog.api.2gis.com/3.0/items"
 rubric_id = '161'  # Замените на актуальный rubric_id
-api_key = '013f41f3-bf5c-45e6-b9ad-91037a03cb13'  # Замените на актуальный api_key
+api_key = ''  # Замените на актуальный api_key
 
 params = {
     'rubric_id': rubric_id,
@@ -34,34 +33,34 @@ def find_similar(row):
     rating = row['Rating']
     rubric = row['Rubric']
     context = row['Context']
-    
+
     # Поиск остальных строк
     similar_row = None
     max_similarity = -1
-    
+
     for _, candidate in df.iterrows():
         # Игнорируем саму строку
         if candidate['ID'] == row['ID'] or candidate['Name'] == row['Name']:
             continue
-        
+
         # Сравнения по рейтингам
         # rating_difference = abs(candidate['Rating'] - rating)
-        
+
         # Сравнение рубрик
         rubric_similarity = difflib.SequenceMatcher(None, rubric, candidate['Rubric']).ratio()
-        
+
         # Сравнение контекстов
         context_similarity = difflib.SequenceMatcher(None, context, candidate['Context']).ratio()
-        
+
         # Веса для каждого условия (можно настроить)
         # total_similarity = (1 / (1 + rating_difference)) + rubric_similarity + context_similarity
         total_similarity = rating + rubric_similarity + context_similarity
-        
+
         # Сравниваем с максимальным
         if total_similarity > max_similarity:
             max_similarity = total_similarity
             similar_row = candidate
-    
+
     return similar_row
 
 # Создание нового DataFrame для результатов
